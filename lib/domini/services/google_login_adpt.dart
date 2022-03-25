5143497438080d9cd77274f92c48bfe414f7784f
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-25 21:27:08 +0100

    Working login with Web

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -1,33 +1,34 @@
+import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import '../../misc/user_preferences.dart';
 
 class GoogleLoginAdpt {
   static final _instance = GoogleLoginAdpt._internal();
   var googleMapApiKey = "***REMOVED***";
-  static final _googleSignIn = GoogleSignIn();
-
-  //User info
-  var _id;
-  var name;
-  var email;
-  var photoUrl;
+  static final _clientIDWeb= "***REMOVED***";
+  static final _clientIDAndroid = "***REMOVED***";
+  static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb);
 
   factory GoogleLoginAdpt() {
     return _instance;
   }
 
   GoogleLoginAdpt._internal();
+  CtrlPresentation ctrlPresentation = CtrlPresentation();
 
   Future<GoogleSignInAccount?> login() {
       final user = _googleSignIn.signIn();
       user.then((u) {
-        _id = u?.id;
-        name = u?.displayName;
-        email = u?.email;
-        photoUrl = u?.photoUrl;
-        print(u);
+        ctrlPresentation.name = u?.displayName;
+        ctrlPresentation.email = u?.email;
+        ctrlPresentation.photoUrl = u?.photoUrl;
+        print(ctrlPresentation.photoUrl);
       });
       return user;
+  }
+
+  Future<GoogleSignInAccount?> logout() {
+    final user = _googleSignIn.disconnect();
+    return user;
   }
 
 
