211a7f3771bb89a6e2e2eaaf02e99951840745f2
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-25 15:20:29 +0100

    WIP login API

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -3,6 +3,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 class GoogleLoginAdpt {
   static final _instance = GoogleLoginAdpt._internal();
   var googleMapApiKey = "***REMOVED***";
+  static final _googleSignIn = GoogleSignIn();
 
   factory GoogleLoginAdpt() {
     return _instance;
@@ -10,6 +11,7 @@ class GoogleLoginAdpt {
 
   GoogleLoginAdpt._internal();
 
+  Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
 
 
 }
