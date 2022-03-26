9227029cf7f3d8e706287565110c0e9327ecd3dc
Alexandru Dumitru <alexandru666@outlook.es>
2022-03-26 16:14:25 +0100

    sign in completly fixed

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -1,11 +1,14 @@
 import 'package:flutter_project/interficie/ctrl_presentation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/foundation.dart';
 
 class GoogleLoginAdpt {
   static final _instance = GoogleLoginAdpt._internal();
   static final _clientIDWeb= "***REMOVED***";
   static final _clientIDAndroid = "***REMOVED***";
-  static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb);
+
+  static final _googleSignInAndroid = GoogleSignIn();
+  static final _googleSignInWeb = GoogleSignIn(clientId: _clientIDWeb);
 
   factory GoogleLoginAdpt() {
     return _instance;
@@ -15,6 +18,10 @@ class GoogleLoginAdpt {
   CtrlPresentation ctrlPresentation = CtrlPresentation();
 
   Future<GoogleSignInAccount?> login() {
+    var _googleSignIn;
+    if (defaultTargetPlatform == TargetPlatform.android) _googleSignIn = _googleSignInAndroid;
+    else _googleSignIn = _googleSignInWeb;
+
     print("aaaaaaaaaaaa\n\n\n\n");
       final user = _googleSignIn.signIn();
       user.then((u) {
@@ -27,11 +34,19 @@ class GoogleLoginAdpt {
   }
 
   Future<GoogleSignInAccount?> logout() {
+    var _googleSignIn;
+    if (defaultTargetPlatform == TargetPlatform.android) _googleSignIn = _googleSignInAndroid;
+    else _googleSignIn = _googleSignInWeb;
+
     final user = _googleSignIn.signOut();
     return user;
   }
 
   Future<bool> isSignIn() {
+    var _googleSignIn;
+    if (defaultTargetPlatform == TargetPlatform.android) _googleSignIn = _googleSignInAndroid;
+    else _googleSignIn = _googleSignInWeb;
+
     return _googleSignIn.isSignedIn();
   }
 
