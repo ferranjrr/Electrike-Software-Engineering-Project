7df181dfbe286d6c41a2db95d6e9f9d78efb9cfb
Víctor Asenjo <victorasenj@gmail.com>
2022-03-26 13:15:50 +0100

    login and logout done

--- lib/domini/services/google_login_adpt.dart
+++ lib/domini/services/google_login_adpt.dart
@@ -5,8 +5,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleLoginAdpt {
   static final _instance = GoogleLoginAdpt._internal();
-  static final _clientIDWeb= "***REMOVED***";
-  static final _clientIDAndroid = "***REMOVED***";
+  static const _clientIDWeb= "***REMOVED***";
+  static const _clientIDAndroid = "***REMOVED***";
   static final _googleSignIn = GoogleSignIn(clientId: _clientIDWeb);
 
   factory GoogleLoginAdpt() {
@@ -17,13 +17,11 @@ class GoogleLoginAdpt {
   CtrlPresentation ctrlPresentation = CtrlPresentation();
 
   Future<GoogleSignInAccount?> login() {
-    print("aaaaaaaaaaaa\n\n\n\n");
       final user = _googleSignIn.signIn();
       user.then((u) {
         ctrlPresentation.name = u?.displayName;
         ctrlPresentation.email = u?.email;
         ctrlPresentation.photoUrl = u?.photoUrl;
-        print(u);
       });
       return user;
   }
